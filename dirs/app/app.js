4802985e4b1660f750b4fecea6ec85169dd1d58a
abdellah karani <abdellahkarani@gmail.com>
2024-11-23 18:29:46 +0100

    test my idea

--- dirs/app/app.js
+++ dirs/app/app.js
@@ -1,1 +1,13 @@
 // Not Implmented Yet
+
+let name = document.querySelector(".name");
+
+if (JSON.parse(localStorage.getItem('PersonalInfo'))) {
+    let storedArray = JSON.parse(localStorage.getItem('PersonalInfo'));
+
+    console.log(storedArray);
+    name.textContent = storedArray;
+}
+
+
+
--- dirs/login/login.js
+++ dirs/login/login.js
@@ -1,1 +1,20 @@
 // write here for login
+
+let submit = document.querySelector(".submit");
+let name = document.querySelector(".name");
+
+submit.addEventListener("click", function() {
+    console.log(name.value);
+
+    localStorage.setItem('PersonalInfo', JSON.stringify(name.value));
+
+    let storedArray = JSON.parse(localStorage.getItem('PersonalInfo'));
+
+    console.log(storedArray);
+
+
+});
+
+
+
+
