9e73f55dd1817de2fa200cf88abe97c03cb36f64
abdellah karani <abdellahkarani@gmail.com>
2024-11-25 21:04:18 +0100

    finich 80% of the fiirst task

--- dirs/login/login.js
+++ dirs/login/login.js
@@ -1,5 +1,31 @@
 
+// Mokey Expression
 let login_btn = document.querySelector(".login-btn");
+let icon_monekey = document.querySelector(".icon-monekey");
+let user_email = document.querySelector(".user_email");
+let user_password = document.querySelector(".user_password");
+let eles = [user_email, user_password, login_btn];
+let imojies = ["🙉", "🙈", "🙊"];
+eles.forEach(element => {
+    element.addEventListener("click", function() {
+        icon_monekey.textContent = imojies[eles.indexOf(element)];
+    });
+});
 
+login_btn.addEventListener("click", function() {
+    if (!user_email.value || !user_password.value) {
+        // Add danger Class
+        user_email.classList.add("danger");
+        user_password.classList.add("danger");
+    } else {
+        // Add success Class
+        user_email.classList.add("success");
+        user_password.classList.add("success");
 
+        // Verifierr the localStorage
+        
+
+        window.location.href = "../../index.html";
+    }
+});
 
