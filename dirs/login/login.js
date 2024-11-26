521939220691a34699f074b6fbf1c4b0660548f9
abdellah karani <abdellahkarani@gmail.com>
2024-11-26 21:36:21 +0100

    add images

--- dirs/login/login.js
+++ dirs/login/login.js
@@ -4,6 +4,8 @@ let login_btn = document.querySelector(".login-btn");
 let icon_monekey = document.querySelector(".icon-monekey");
 let user_email = document.querySelector(".user_email");
 let user_password = document.querySelector(".user_password");
+
+
 let eles = [user_email, user_password, login_btn];
 let imojies = ["🙉", "🙈", "🙊"];
 eles.forEach(element => {
@@ -22,10 +24,14 @@ login_btn.addEventListener("click", function() {
         user_email.classList.add("success");
         user_password.classList.add("success");
 
-        // Verifierr the localStorage
-        
-
-        window.location.href = "../../index.html";
+        // Login Verification
+        const MyInfo = JSON.parse(localStorage.getItem("PersonlInfo"));
+        let user_email_local = MyInfo.user_email;
+        let user_password_local = MyInfo.user_password;
+        console.log(user_email, user_password);
+        if (user_email.value == user_email_local && user_password_local == user_password.value) {
+            window.location.href = "../../index.html";
+        }
     }
 });
 
