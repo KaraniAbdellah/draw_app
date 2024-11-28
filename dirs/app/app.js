350033219ff8e6d844c165a70b4b3dcd28a8c2d1
abdellah karani <abdellahkarani@gmail.com>
2024-11-28 19:44:28 +0100

    remove shapes

--- dirs/app/app.js
+++ dirs/app/app.js
@@ -1,13 +1,5 @@
 // Not Implmented Yet
 
-let name = document.querySelector(".name");
-
-if (JSON.parse(localStorage.getItem('PersonalInfo'))) {
-    let storedArray = JSON.parse(localStorage.getItem('PersonalInfo'));
-
-    console.log(storedArray);
-    name.textContent = storedArray;
-}
 
 
 
